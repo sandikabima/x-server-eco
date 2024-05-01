6df273d852ba5272f0128a57155174a83e0f358c
Bima sandika <bimasandika@gmail.com>
2024-05-01 13:31:07 -0700

    x-ser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const sequelize = require('./utils/conn')
 const app = express();
 const port = 3000;
 
@@ -6,6 +7,16 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('ini di server')
 })
+
+app.get('/test', async (req, res) => {
+    try {
+        await sequelize.authenticate();
+        res.send('Connection has been established successfully.');
+      } catch (error) {
+        res.send('Unable to connect to the database:', error);
+      }
+})
+
 
 app.listen(port, () => {
     console.log('Server listening in port', port);
---
+++ src/utils/conn.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require('sequelize')
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host : process.env.DB_HOST,
+        port : process.env.DB_PORT,
+        dialect : process.env.DB_DIALECT,
+        logging : process.env.NODE_ENV === "development" ? (...msg) => console.log(msg) : false,
+        dialectOptions : {
+            requestTimeout : 3000,
+            encrypt : true,
+        } 
+    }
+)
+
+module.exports = sequelize
