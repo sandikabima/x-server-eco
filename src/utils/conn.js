27ae8e6b4b11f664cfab1edcc1fd8f849055ffcf
Bima sandika <bimasandika@gmail.com>
2024-05-01 13:34:29 -0700

    x-ser

--- src/utils/conn.js
+++ src/utils/conn.js
@@ -1,4 +1,5 @@
 const {Sequelize} = require('sequelize')
+const pg = require('pg')
 require('dotenv').config()
 
 const sequelize = new Sequelize(
@@ -9,6 +10,7 @@ const sequelize = new Sequelize(
         host : process.env.DB_HOST,
         port : process.env.DB_PORT,
         dialect : process.env.DB_DIALECT,
+        dialectModule: pg,
         logging : process.env.NODE_ENV === "development" ? (...msg) => console.log(msg) : false,
         dialectOptions : {
             requestTimeout : 3000,
